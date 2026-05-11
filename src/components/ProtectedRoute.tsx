ab02b4b067084c953105e43d2598d7fc7a2592fe
Ng Sean Sean <23004914@siswa.um.edu.my>
2026-05-11 18:15:53 +0800

    Revert "feat: add Projects and Reports pages with filtering and score tracking"
    This reverts commit 5e743eb5b2ed72683f019952b6ccdf03131521f2.

--- src/components/ProtectedRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import { useStore } from '../store/useStore'
-
-export default function ProtectedRoute() {
-  const isAuthenticated = useStore((s) => s.isAuthenticated)
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />
-}
